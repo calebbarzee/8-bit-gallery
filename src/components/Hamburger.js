c0b8a4ae86f6a5639de1de9661cddf3e9b8a92d3
trevorlamoglia <trevorlamoglia21@gmail.com>
2022-07-13 18:59:46 -0600

    hamburger comments

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Link } from "react-router-dom";
 import Home from '../pages/Home';
 import './Navigation.css'
 
+// We set the initial state to false. By default, we want to hide the sidebar navigation. 
+// Then, when the menu button is clicked, we can then call the updater function, setNavbarOpen, to update the state.
 function Hamburger(){
     const [navbarOpen, setNavbarOpen] = useState(false);
 
@@ -14,12 +16,14 @@ function Hamburger(){
 
     return (
         <nav className="navBar">
+            {/* Code for button. Line 19-21 is the code for the menu style */}
             <button onClick={handleToggle}>{navbarOpen ? (
                 <MdClose style={{ background: "#FFFFFF", color: "#000000", width: "20px", height: "20px" }}></MdClose>
             ) : (
                 <MdMenu style={{ background: "#FFFFFF", color: "#000000", width: "20px", height: "20px" }} />
             )}</button>
             
+            {/* Navigation in the menu. */}
             <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
                 <li>
                     <Link to="/home" className="navlink">Home</Link>
